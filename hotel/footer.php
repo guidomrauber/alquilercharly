8620ba95e52ba8f5b3c5d36719b30ed06f9d9458
guidomrauber <49770318+guidomrauber@users.noreply.github.com>
2022-09-17 07:55:15 -0300

    Add files via upload

--- hotel/footer.php
+++ hotel/footer.php
@@ -39,14 +39,30 @@ Los esperamos en Godoy Cruz 55 y 61 entre San Martín y 9 de julio </p>
         <!--/.container-->    
     
     <!--/.footer-bottom--> 
+    
 </footer>
-
-<div class="text-center copyright">© 2022 Todos los derechos reservados creado por Guido M Rauber</a></div>
-
-<a href="#home" class="toTop scroll"><i class="fa fa-angle-up"></i></a>
-
-
-
+<div class="text-center copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<p class="company-name">Todos los derechos reservados . &copy; 2022 
+                            <a href="https://www.linkedin.com/in/guido-martin-rauber-511128187/">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" 
+                            width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" 
+                            stroke-linecap="round" stroke-linejoin="round">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+							<rect x="4" y="4" width="16" height="16" rx="2" />
+							<line x1="8" y1="11" x2="8" y2="16" />
+							<line x1="8" y1="8" x2="8" y2="8.01" />
+							<line x1="12" y1="16" x2="12" y2="11" />
+							<path d="M16 16v-3a2 2 0 0 0 -4 0" />
+						  </svg> </a> Diseñado por : 
+					<a href="https://html.design/">Guido M Rauber</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
 
 <!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
 <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
